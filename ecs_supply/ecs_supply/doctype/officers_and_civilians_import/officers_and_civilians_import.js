bf6c5fd11e91a2ec325d55add16ad6c50e3af9e9
erpcloudsystems <mghoneem10@gmail.com>
2023-09-19 12:33:30 +0000

    migration to server

---
+++ ecs_supply/ecs_supply/doctype/officers_and_civilians_import/officers_and_civilians_import.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2023, erpcloud.systems and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Officers and Civilians Import", {
+// 	attach_file(frm) {
+//         frm.call({
+//             doc: frm.doc,
+//             method:"attach_file",
+//             freeze:true,
+//             freeze_message:"جاري إرفاق الملف ....",
+//             callback:function(r){
+//                 console.log(r)
+//             }
+//         })
+// 	},
+// });
+
---
+++ ecs_supply/ecs_supply/doctype/officers_and_civilians_import/officers_and_civilians_import.py
@@ -0,0 +1,32 @@
+# Copyright (c) 2023, erpcloud.systems and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+import pyodbc
+
+import os
+
+#to get the current working directory
+
+
+class OfficersandCiviliansImport(Document):
+	def validate(self):
+		directory = os.getcwd()
+		frappe.msgprint(self.attach_file)
+		# file_path = directory + self.attach_file
+		# frappe.msgprint(file_path)
+		# db_driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
+		# db_path = '//10.0.0.5/ex_backup2/frappe/frappe-bench/sites/erp.emdad.local/public/files/افراد (1).mdb'
+		import pandas_access as mdb
+		os.chdir('/private/files')
+
+		db_filename = 'افراد (1).mdb'
+		# Listing the tables.
+		for tbl in mdb.list_tables(db_filename):
+			frappe.msgprint(tbl)
+		# Read a small table.
+		df = mdb.read_table(db_filename, "OFF_PER_DATA")
+		frappe.msgprint(str(df))
+
+		# frappe.msgprint(self.attach_file)
