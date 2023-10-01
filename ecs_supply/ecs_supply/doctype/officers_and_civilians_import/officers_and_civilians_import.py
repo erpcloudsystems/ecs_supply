10a99219c75d59a806c05d7ad6c6543d34dd4cb8
mghoneem10 <mhgoneem10@gmail.com>
2023-10-01 07:53:32 +0000

    1-10-23 10.53 am

--- ecs_supply/ecs_supply/doctype/officers_and_civilians_import/officers_and_civilians_import.py
+++ ecs_supply/ecs_supply/doctype/officers_and_civilians_import/officers_and_civilians_import.py
@@ -3,7 +3,7 @@
 
 import frappe
 from frappe.model.document import Document
-import pyodbc
+# import pyodbc
 
 import os
 
@@ -13,20 +13,20 @@ import os
 class OfficersandCiviliansImport(Document):
 	def validate(self):
 		directory = os.getcwd()
-		frappe.msgprint(self.attach_file)
-		# file_path = directory + self.attach_file
-		# frappe.msgprint(file_path)
-		# db_driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
-		# db_path = '//10.0.0.5/ex_backup2/frappe/frappe-bench/sites/erp.emdad.local/public/files/افراد (1).mdb'
-		import pandas_access as mdb
-		os.chdir('/private/files')
+	# 	frappe.msgprint(self.attach_file)
+	# 	# file_path = directory + self.attach_file
+	# 	# frappe.msgprint(file_path)
+	# 	# db_driver = '{Microsoft Access Driver (*.mdb, *.accdb)}'
+	# 	# db_path = '//10.0.0.5/ex_backup2/frappe/frappe-bench/sites/erp.emdad.local/public/files/افراد (1).mdb'
+	# 	import pandas_access as mdb
+	# 	os.chdir('/private/files')
 
-		db_filename = 'افراد (1).mdb'
-		# Listing the tables.
-		for tbl in mdb.list_tables(db_filename):
-			frappe.msgprint(tbl)
-		# Read a small table.
-		df = mdb.read_table(db_filename, "OFF_PER_DATA")
-		frappe.msgprint(str(df))
+	# 	db_filename = 'افراد (1).mdb'
+	# 	# Listing the tables.
+	# 	for tbl in mdb.list_tables(db_filename):
+	# 		frappe.msgprint(tbl)
+	# 	# Read a small table.
+	# 	df = mdb.read_table(db_filename, "OFF_PER_DATA")
+	# 	frappe.msgprint(str(df))
 
 		# frappe.msgprint(self.attach_file)
