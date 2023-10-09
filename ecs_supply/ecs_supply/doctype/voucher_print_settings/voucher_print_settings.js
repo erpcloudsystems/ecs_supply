e4c61aa88ff4d372079948dc81cf20eaf0ac9cef
ERP CLOUD SYSTEMS <info@erpcloud.systems>
2023-10-09 08:47:59 +0200

    09-10-2023 9:48 AM

--- ecs_supply/ecs_supply/doctype/voucher_print_settings/voucher_print_settings.js
+++ ecs_supply/ecs_supply/doctype/voucher_print_settings/voucher_print_settings.js
@@ -17,7 +17,6 @@ frappe.ui.form.on('Voucher Print Settings', {
             console.log(frm.fields_dict)
 			$(frm.fields_dict.html_wcyq.wrapper).empty()
 
-
 			var template = `<div style="position: relative;">\
                     <div id="cheque_preview" style="width: 210.00mm; \
                         height:297.00mm;\
@@ -25,61 +24,80 @@ frappe.ui.form.on('Voucher Print Settings', {
                         background-image: url('{{ scan_image }}') ;\
                         background-size: cover;
                         position: relative;
+                        color: darkblue;
                         font-weight: bold;
                         ">\
                         \
                         <span style="right: 0; margin: {{ bon_settings[0]['vertical'] + voucher1_space }}mm {{ bon_settings[0]['horizontal'] }}mm 0 0; 
-                            position: absolute;">  {{ bon_settings[0]['fieldname'] }} </span>\
+                            position: absolute;">  {{ bon_settings[0]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[1]['vertical'] + voucher1_space }}mm {{ bon_settings[1]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[1]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[1]['example'].split("-")[2] }} &nbsp; {{ bon_settings[1]['example'].split("-")[1] }} &nbsp; {{ bon_settings[1]['example'].split("-")[0] }}</span>\
                         <span style="right: 0; margin: {{ bon_settings[2]['vertical'] + voucher1_space }}mm {{ bon_settings[2]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[2]['fieldname'] }} </span>\
+                        position: absolute;">  
+                        {% if print_name_with_rank %}
+                        {{ bon_settings[2]['example'] }}/ {{ footer_settings[0]['example'] }}
+                        {% else %}
+                        {{ bon_settings[2]['example'] }}
+                        {% endif %}
+                         </span>
                         <span style="right: 0; margin: {{ bon_settings[3]['vertical'] + voucher1_space }}mm {{ bon_settings[3]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[3]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[3]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[4]['vertical'] + voucher1_space }}mm {{ bon_settings[4]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[4]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[4]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[5]['vertical'] + voucher1_space }}mm {{ bon_settings[5]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[5]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[5]['example'].split("-")[2] }} &nbsp; {{ bon_settings[5]['example'].split("-")[1] }} &nbsp; {{ bon_settings[5]['example'].split("-")[0] }}</span>\
 
 
                         <span style="right: 0; margin: {{ bon_settings[0]['vertical'] + voucher2_space }}mm {{ bon_settings[0]['horizontal'] }}mm 0 0; 
-                            position: absolute;">  {{ bon_settings[0]['fieldname'] }} </span>\
+                            position: absolute;">  {{ bon_settings[0]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[1]['vertical'] + voucher2_space }}mm {{ bon_settings[1]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[1]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[1]['example'].split("-")[2] }} &nbsp; {{ bon_settings[1]['example'].split("-")[1] }} &nbsp; {{ bon_settings[1]['example'].split("-")[0] }}</span>\
                         <span style="right: 0; margin: {{ bon_settings[2]['vertical'] + voucher2_space }}mm {{ bon_settings[2]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[2]['fieldname'] }} </span>\
+                        position: absolute;">
+                        {% if print_name_with_rank %}
+                        {{ bon_settings[2]['example'] }}/ {{ footer_settings[0]['example'] }}
+                        {% else %}
+                        {{ bon_settings[2]['example'] }}
+                        {% endif %}
+                        </span>\
                         <span style="right: 0; margin: {{ bon_settings[3]['vertical'] + voucher2_space }}mm {{ bon_settings[3]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[3]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[3]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[4]['vertical'] + voucher2_space }}mm {{ bon_settings[4]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[4]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[4]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[5]['vertical'] + voucher2_space }}mm {{ bon_settings[5]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[5]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[5]['example'].split("-")[2] }} &nbsp; {{ bon_settings[5]['example'].split("-")[1] }} &nbsp; {{ bon_settings[5]['example'].split("-")[0] }}</span>\
 
 
                         <span style="right: 0; margin: {{ bon_settings[0]['vertical'] + voucher3_space }}mm {{ bon_settings[0]['horizontal'] }}mm 0 0; 
-                            position: absolute;">  {{ bon_settings[0]['fieldname'] }} </span>\
+                            position: absolute;">  {{ bon_settings[0]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[1]['vertical'] + voucher3_space }}mm {{ bon_settings[1]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[1]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[1]['example'].split("-")[2] }} &nbsp; {{ bon_settings[1]['example'].split("-")[1] }} &nbsp; {{ bon_settings[1]['example'].split("-")[0] }}</span>\
                         <span style="right: 0; margin: {{ bon_settings[2]['vertical'] + voucher3_space }}mm {{ bon_settings[2]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[2]['fieldname'] }} </span>\
+                        position: absolute;">
+                        {% if print_name_with_rank %}
+                        {{ bon_settings[2]['example'] }}/ {{ footer_settings[0]['example'] }}
+                        {% else %}
+                        {{ bon_settings[2]['example'] }}
+                        {% endif %}
+                        </span>\
                         <span style="right: 0; margin: {{ bon_settings[3]['vertical'] + voucher3_space }}mm {{ bon_settings[3]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[3]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[3]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[4]['vertical'] + voucher3_space }}mm {{ bon_settings[4]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[4]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[4]['example'] }} </span>\
                         <span style="right: 0; margin: {{ bon_settings[5]['vertical'] + voucher3_space }}mm {{ bon_settings[5]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ bon_settings[5]['fieldname'] }} </span>\
+                        position: absolute;">  {{ bon_settings[5]['example'].split("-")[2] }} &nbsp; {{ bon_settings[5]['example'].split("-")[1] }} &nbsp; {{ bon_settings[5]['example'].split("-")[0] }}</span>\
 
 
                         <span style="right: 0; margin: {{ footer_settings[0]['vertical'] + voucher_footer_space }}mm {{ footer_settings[0]['horizontal'] }}mm 0 0; 
-                            position: absolute;">  {{ footer_settings[0]['fieldname'] }} </span>\
+                            position: absolute;">  {{ footer_settings[0]['example'] }} </span>\
                         <span style="right: 0; margin: {{ footer_settings[1]['vertical'] + voucher_footer_space }}mm {{ footer_settings[1]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ footer_settings[1]['fieldname'] }} </span>\
+                        position: absolute;">  {{ footer_settings[1]['example'] }} </span>\
                         <span style="right: 0; margin: {{ footer_settings[2]['vertical'] + voucher_footer_space }}mm {{ footer_settings[2]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ footer_settings[2]['fieldname'] }} </span>\
+                        position: absolute;">  {{ footer_settings[2]['example'] }} </span>\
                         <span style="right: 0; margin: {{ footer_settings[3]['vertical'] + voucher_footer_space }}mm {{ footer_settings[3]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ footer_settings[3]['fieldname'] }} </span>\
+                        position: absolute;">  {{ footer_settings[3]['example'] }} </span>\
                         <span style="right: 0; margin: {{ footer_settings[4]['vertical'] + voucher_footer_space }}mm {{ footer_settings[4]['horizontal'] }}mm 0 0; 
-                        position: absolute;">  {{ footer_settings[4]['fieldname'] }} </span>\
+                        position: absolute;">  {{ footer_settings[4]['example'].split("-")[2] }} &nbsp; {{ footer_settings[4]['example'].split("-")[1] }} &nbsp; {{ footer_settings[4]['example'].split("-")[0] }}</span>\
     
                     </div>\
 			</div>
